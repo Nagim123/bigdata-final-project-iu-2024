5ff799cc657c1c441c37f0c63408aa11fcc02b20
unknown <firasjolha@gmail.com>
2023-03-22 09:37:43 +0300

    update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,20 @@
+#!/bin/sh
+
+
+# Run the big data pipeline
 bash scripts/run.sh
+
+
+# Check the quality of the codes
+echo "The quality of scripts in 'scripts/' folder\n"
+echo "::============================================::"
+pylint scripts
+
+
+echo "The quality of scripts in 'notebooks/' folder"
+echo "::============================================::"
+pylint notebooks
+
+
+
+echo "Done testing the pipeline!"
