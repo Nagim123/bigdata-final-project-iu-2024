8230c8197d3d4b79ad118f390011bfaa6ad090cb
Firas Jolha <36399502+firas-jolha@users.noreply.github.com>
2023-03-22 10:31:21 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,20 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 
 # Run the big data pipeline
-bash scripts/run.sh
+echo "Running Stage 1 of the pipeline"
+bash scripts/stage1.sh
+
+echo "Running Stage 2 of the pipeline"
+bash scripts/stage2.sh
+
+echo "Running Stage 3 of the pipeline"
+bash scripts/stage3.sh
+
+echo "Running Stage 4 of the pipeline"
+bash scripts/stage4.sh
+
+echo "Running Stage 5 of the pipeline"
+bash scripts/stage5.sh
+
+
+#bash scripts/run.sh
 
 
 # Check the quality of the codes
 echo "The quality of scripts in 'scripts/' folder\n"
 echo "::============================================::"
 pylint scripts
-
-
-echo "The quality of scripts in 'notebooks/' folder"
-echo "::============================================::"
-pylint notebooks
-
 
 
 echo "Done testing the pipeline!"
